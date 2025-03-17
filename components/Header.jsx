93b0b74bb81ba35033841827c01c24a976aab0b3
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2025-03-17 15:40:27 +0500

    Resolved conflicts and merged

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <header>
+      <img src="/images/globe.png" alt="globe img" />
+      <h1>my travel journal</h1>
+    </header>
+  );
+}
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/**
+ * Challenge:
+ *
+ * - Create an App component in a separate file.
+ *   Import it here and render it
+ * - Create a "components" folder and a Header component.
+ *   Render the Header inside the App component.
+ * - Follow the Travel Journal design to build the Header
+ *   for our page.
+ */
+//import Header from "../components/Header";
+
+import { Header } from "../components/Header";
+
+export default function App() {
+  return <Header />;
+}
